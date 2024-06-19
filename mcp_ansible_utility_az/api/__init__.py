a67b3dcc54a0392b09483d7d48091a0138d8392e
vxrail-ansible-team <vxrail.ansible.team@dell.com>
2024-06-19 02:39:35 +0000

    1.0.0 utility release

---
+++ mcp_ansible_utility_az/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mcp_ansible_utility_az.api.authentication_of_apex_cloud_platform_manager_api import AuthenticationOfAPEXCloudPlatformManagerApi
+from mcp_ansible_utility_az.api.installation_and_deployment_of_apex_cloud_platform_for_microsoft_azure_api import InstallationAndDeploymentOfAPEXCloudPlatformForMicrosoftAzureApi
